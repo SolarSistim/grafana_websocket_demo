8f2570611ab2c87b9fabeb3320be7fa8f565d67d
SolarSistim <47832728+SolarSistim@users.noreply.github.com>
2023-05-30 06:51:04 -0500

    Update websocket_demo.py

--- websocket_demo.py
+++ websocket_demo.py
@@ -1,20 +1,16 @@
 import asyncio
-import random
 import websockets
-from random import randrange
+import random
+import time
 
-async def handle_websocket(websocket, path):
+async def handle_client(websocket, path):
     while True:
-
-        current_number = randrange(100)
-        increment = random.choice([-1, 1])
-        value = random.randint(1, 10) * 5
-        new_number = max(1, min(100, current_number + (increment * value)))
-        current_number = new_number
-        await websocket.send(str(current_number))
-        await asyncio.sleep(0.5)
-
-start_server = websockets.serve(handle_websocket, 'localhost', 8765)
-
+        timestamp = time.time()
+        message = random.randint(1, 100)
+        timestamp = str(timestamp)
+        message = str(message)
+        await websocket.send('{"timestamp": ' + '"' + timestamp + '", "message": "' + message + '"}')
+        await asyncio.sleep(1)
+start_server = websockets.serve(handle_client, 'localhost', 8765)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
